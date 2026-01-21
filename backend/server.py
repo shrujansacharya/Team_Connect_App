bdeab7c546bb111a52ecb2f8c0d88edf9d88ef34
Shrujan S  Acharya <acharyashrujan1@gmail.com>
2026-01-21 09:04:51 +0530

    Fix CORS: Update default origins

--- backend/server.py
+++ backend/server.py
@@ -707,7 +707,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=os.environ.get('CORS_ORIGINS', 'http://localhost:3000,https://team-connect-app.vercel.app').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
